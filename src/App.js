e1039486f4dd78ee94714d463c980df40ef62d6c
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-21 14:49:52 -0300

    feat: app

--- src/App.js
+++ src/App.js
@@ -2,14 +2,24 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import GlobalStyle from "./assets/css/globalStyles";
 import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
+import HomePage from "./components/HomePage";
+import EntryPage from "./components/EntryPage";
+import SpentPage from "./components/SpentPage"
+import { useState } from "react";
 
 export default function App() {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path={"/"} element={<SignIn />} />
+        <Route path={""} element={<SignIn setEmail={setEmail} setName={setName}/>} />
         <Route path={"sign-up"} element={<SignUp />} />
+        <Route path={"home"} element={<HomePage email={email} name={name}/>} />
+        <Route path={"new-entry"} element={<EntryPage email={email} />} />
+        <Route path={"new-spent"} element={<SpentPage email={email} />} />
       </Routes>
     </BrowserRouter>
   );
