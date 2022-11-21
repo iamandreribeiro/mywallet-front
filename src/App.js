fb410a756ee5b3fa2463cf91a9edcf5a6697fc6c
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-11-21 15:50:52 -0300

    refactor: applying context api pattern at app.js

--- src/App.js
+++ src/App.js
@@ -4,23 +4,22 @@ import SignIn from "./components/SignIn";
 import SignUp from "./components/SignUp";
 import HomePage from "./components/HomePage";
 import EntryPage from "./components/EntryPage";
-import SpentPage from "./components/SpentPage"
-import { useState } from "react";
+import SpentPage from "./components/SpentPage";
+import AuthProvider from "./context/auth";
 
 export default function App() {
-  const [email, setEmail] = useState("");
-  const [name, setName] = useState("");
-
   return (
     <BrowserRouter>
       <GlobalStyle />
-      <Routes>
-        <Route path={""} element={<SignIn setEmail={setEmail} setName={setName}/>} />
-        <Route path={"sign-up"} element={<SignUp />} />
-        <Route path={"home"} element={<HomePage email={email} name={name}/>} />
-        <Route path={"new-entry"} element={<EntryPage email={email} />} />
-        <Route path={"new-spent"} element={<SpentPage email={email} />} />
-      </Routes>
+      <AuthProvider>
+        <Routes>
+          <Route path={""} element={<SignIn />} />
+          <Route path={"sign-up"} element={<SignUp />} />
+          <Route path={"home"} element={<HomePage />} />
+          <Route path={"new-entry"} element={<EntryPage />} />
+          <Route path={"new-spent"} element={<SpentPage />} />
+        </Routes>
+      </AuthProvider>
     </BrowserRouter>
   );
 }
